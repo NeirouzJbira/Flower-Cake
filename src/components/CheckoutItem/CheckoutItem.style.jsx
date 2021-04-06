19ad30e81e1dd5992094311ac5fa7fe83deaa58b
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-07 00:17:40 +0200

    init

---
+++ src/components/CheckoutItem/CheckoutItem.style.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+export const CheckoutItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  height: 350px;
+  font-size: 20px;
+  align-items: center;
+  border-bottom: 1px solid #888888;
+  padding: 15px 0;
+  align-items: center;
+
+  @media (max-width: 800px) {
+    font-size: 18px;
+  }
+`;
+
+export const Description = styled.div`
+  width: 23%;
+
+  @media (max-width: 800px) {
+    width: 22%;
+  }
+`;
+
+export const QuantityContainer = styled.div`
+  display: flex;
+  width: 23%;
+
+  div {
+    cursor: pointer;
+  }
+
+  span {
+    margin: 0 10px;
+  }
+`;
+
+export const Image = styled.img`
+  width: 18%;
+  height: 300px;
+  margin-right: 150px;
+
+  @media (max-width: 800px) {
+    width: 25%;
+    margin-right: 10px;
+  }
+`;
+
+export const Price = styled.div`
+  width: 23%;
+`;
+
+export const RemoveButton = styled.div`
+  padding-left: 12px;
+  cursor: pointer;
+`;
