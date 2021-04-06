8406426b3b99003378db2b81e1609db56736cbc6
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-07 00:54:48 +0200

    init

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import HeartIcon from "../HeartIcon/HeartIcon";
+import CartIcon from "../CartIcon/CartIcon";
+import Cart from "../Cart/Cart";
+
+import { MenuContainer, MenuList, MenuItem } from "./Menu.style";
+
+class Menu extends React.Component {
+  render() {
+    return (
+      <MenuContainer>
+        <MenuList>
+          <MenuItem to="/">Home</MenuItem>
+          <MenuItem to="/about">About</MenuItem>
+          <MenuItem to="/store">Store</MenuItem>
+          <MenuItem to="/favorite">
+            {" "}
+            <HeartIcon />{" "}
+          </MenuItem>
+          <CartIcon />
+        </MenuList>
+        {this.props.hideCart ? null : <Cart />}
+      </MenuContainer>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    hideCart: state.cart.hidden,
+  };
+};
+
+export default connect(mapStateToProps)(Menu);
