edf9165ab158018d42010a3d9bbb8dd6301f3824
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-07 00:17:13 +0200

    init

---
+++ src/components/CartIcon/CartIcon.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { showHideCart } from "../../redux/Cart/actions";
+
+import { CartContainer, ShoppingIcon, ItemsCounter } from "./CartIcon.style";
+
+const CartIcon = (props) => (
+  <CartContainer onClick={props.showHideCart}>
+    <ShoppingIcon />
+    <ItemsCounter>{props.cartItemsCount}</ItemsCounter>
+  </CartContainer>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  showHideCart: () => dispatch(showHideCart()),
+});
+
+const mapStateToProps = (state) => {
+  return {
+    cartItemsCount: state.cart.items.length,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
