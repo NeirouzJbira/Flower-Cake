e403bc9b290367cdb5b0c3556a9cd06012b7d119
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-07 00:17:24 +0200

    init

---
+++ src/components/CartItem/CartItem.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import {
+  CartItemContainer,
+  CartItemImage,
+  CartItemDetailsContainer,
+} from "./CartItem.style";
+
+class CartItem extends React.Component {
+  render() {
+    const { name, quantity, price, imageUrl } = this.props.item;
+    return (
+      <CartItemContainer>
+        <CartItemImage src={imageUrl} alt="" />
+        <CartItemDetailsContainer>
+          <span>{name}</span>
+          <span>
+            {" "}
+            {quantity} x ${price}{" "}
+          </span>
+        </CartItemDetailsContainer>
+      </CartItemContainer>
+    );
+  }
+}
+
+export default CartItem;
