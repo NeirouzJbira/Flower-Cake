e6c665064bf1c9302b53473be205fd2c7d754b23
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-08 23:51:10 +0200

    init

--- src/pages/Checkout/Checkout.style.jsx
+++ src/pages/Checkout/Checkout.style.jsx
@@ -1,44 +1,40 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CheckoutPageContainer = styled.div`
-    color: #666666;
+  color: #666666;
 `;
 
-
 export const Title = styled.h3`
-    text-align: center;
-    font-size: 50px;
-    color: gray;
-    font-family: "Playfair Display";
+  text-align: center;
+  font-size: 50px;
+  color: gray;
+  font-family: "Playfair Display";
 `;
 
 export const ProductsTable = styled.div`
-    width: 80%;
-    display: flex;
-    flex-direction: column;
-    margin: 50px auto;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  margin: 50px auto;
 `;
 
 export const HeaderContainer = styled.div`
-    display: flex;
-    justify-content: space-between;
-    color: #888888;
-    border-bottom: 1px solid #888888;
+  display: flex;
+  justify-content: space-between;
+  color: #888888;
+  border-bottom: 1px solid #888888;
 `;
 
 export const HeaderTitle = styled.div`
-    margin: 1px auto;
+  margin: 1px auto;
 `;
 
-export const ProductsContainer = styled.div`
-
-`;
+export const ProductsContainer = styled.div``;
 
 export const TotalContainer = styled.div`
-    font-size: 30px;
-    text-align: right;
-    margin: 30px 200px;
-    
+  font-size: 30px;
+  text-align: right;
+  margin: 30px 200px;
 `;
 
 export const WarningContainer = styled.div`
@@ -49,7 +45,7 @@ export const WarningContainer = styled.div`
 `;
 
 export const StripeButtonContainer = styled.div`
-    font-size: 30px;
-    text-align: right;
-    margin: 30px 200px;
+  font-size: 30px;
+  text-align: right;
+  margin: 30px 200px;
 `;
