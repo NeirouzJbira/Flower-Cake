56046fcf2304ca5f95d70f9747790b29cb17396b
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-09 20:08:06 +0200

    init

---
+++ src/App.style.jsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components'
+
+
+export const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: "Playfair Display";
+        background-color: #f3f2f0
+    }
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+}
+`;
