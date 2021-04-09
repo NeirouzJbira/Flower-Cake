054d9ab1d0f3cf8a6bab311502bf1b48d905fa8f
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-09 20:08:34 +0200

    init

---
+++ src/redux/Cart/actionTypes.js
@@ -0,0 +1,9 @@
+const CartActionTypes = {
+    SHOW_HIDE_CART: 'SHOW_HIDE_CART',
+    ADD_TO_CART: 'ADD_TO_CART',
+    REMOVE_FROM_CART: 'REMOVE_FROM_CART',
+    CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART',
+    EMPTY_CART: 'EMPTY_CART'
+}
+
+export default CartActionTypes;
