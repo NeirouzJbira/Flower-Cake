381af362b0876341cc08d9997a3d4af3492f783a
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-09 20:08:26 +0200

    init

---
+++ src/redux/Cart/actions.js
@@ -0,0 +1,30 @@
+import CartActionTypes from "./actionTypes";
+
+export const showHideCart = () => ({
+  type: CartActionTypes.SHOW_HIDE_CART,
+});
+
+export const addToCart = (item) => {
+  return {
+    type: CartActionTypes.ADD_TO_CART,
+    payload: item,
+  };
+};
+
+export const removeFromCart = (item) => {
+  return {
+    type: CartActionTypes.REMOVE_FROM_CART,
+    payload: item,
+  };
+};
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
+
+export const emptyCart = () => {
+  return {
+    type: CartActionTypes.EMPTY_CART,
+  };
+};
