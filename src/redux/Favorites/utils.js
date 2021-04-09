fc28a27eed162ddf52256cf07a36dcbb105c4e94
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-09 20:09:07 +0200

    init

---
+++ src/redux/Favorites/utils.js
@@ -0,0 +1,10 @@
+export const addFavoriteItem = (favoriteItems, newItem) => {
+  const itemExist = favoriteItems.filter((item) => item.id === newItem.id);
+  if (itemExist.length > 0) return [...favoriteItems];
+
+  return [...favoriteItems, { ...newItem }];
+};
+
+export const removeFavoriteItem = (favoriteItems, itemToRemove) => {
+  return favoriteItems.filter((item) => item.id !== itemToRemove.id);
+};
