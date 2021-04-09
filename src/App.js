d532c6a7c065dce2274f00a573208b7a7e3de103
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-04-09 20:07:59 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GlobalStyle } from "./App.style";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Store from "./pages/Store/Store";
+import Favorites from "./pages/Favorites/Favorites";
+import Checkout from "./pages/Checkout/Checkout";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <GlobalStyle />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/favorite" component={Favorites} />
+          <Route path="/store" component={Store} />
+          <Route path="/about" component={About} />
+          <Route path="/checkout" component={Checkout} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
